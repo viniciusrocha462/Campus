48b8b5387b628a5d2e30c37af6b21343c9369576
raniery <raniery.peixoto@hotmail.com>
2019-02-14 14:25:28 -0200

    Inicio das paginas de curso, pagina html 5 feita

--- course-single.php
+++ course-single.php
@@ -8,7 +8,7 @@
       include_once "layout/header.php";
       ?>
 
-    <section class="site-hero overlay" data-stellar-background-ratio="0.5" style="background-image: url(images/webdesign.jpg);">
+    <section class="site-hero overlay" data-stellar-background-ratio="0.5" style="background-image: url(images/html.jpg);">
       <div class="container">
         <div class="row align-items-center site-hero-inner justify-content-center">
           <div class="col-md-12">
@@ -16,14 +16,14 @@
             <div class="mb-5 element-animate">
               <div class="row align-items-center">
                 <div class="col-md-8">
-                  <h1 class="mb-0">Web Design 101</h1>
-                  <p>By Gregg White</p>
-                  <p class="lead mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis, fuga.</p>
+                  <h1 class="mb-0">HTML 5</h1>
+                  <p>Por Vinicius Rocha</p>
+                  <p class="lead mb-5">Curso completo sobre HTML 5</p>
                   
-                  <p><a href="#" class="btn btn-primary mr-2">Start Series</a> <a href="#" class="btn btn-outline-white">Add To Watch List</a></p>
+                  <p><a href="#" class="btn btn-primary mr-2">Começar Curso</a> <a href="#" class="btn btn-outline-white">Assistir mais tarde</a></p>
                 </div>
                 <div class="col-md-4">
-                  <img src="images/webdesign.jpg" alt="Image placeholder" class="img-fluid">
+                  <img src="images/html.jpg" alt="Image placeholder" class="img-fluid">
                 </div>
               </div>
             </div>
@@ -101,8 +101,8 @@
       <div class="container">
         <div class="row justify-content-center mb-5">
           <div class="col-md-7 text-center">
-            <h2>You May Also Like</h2>
-            <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum magnam illum maiores adipisci pariatur, eveniet.</p>
+            <h2>Veja também</h2>
+            <p class="lead">Algum dos cursos disponíveis em nosso site</p>
           </div>
         </div>
       
@@ -110,44 +110,44 @@
         <div class="row top-course">
           <div class="col-lg-2 col-md-4 col-sm-6 col-12">
             <a href="#" class="course">
-              <img src="images/webdesign.jpg" alt="Image placeholder">
-              <h2>Web Design 101</h2>
-              <p>Enroll Now</p>
+              <img src="images/html.jpg" alt="Image placeholder">
+              <h2>HTML 5</h2>
+              <p>Vinicius Rocha</p>
             </a>
           </div>
           <div class="col-lg-2 col-md-4 col-sm-6 col-12">
             <a href="#" class="course">
-              <img src="images/wordpress.jpg" alt="Image placeholder">
-              <h2>Learn How To Develop WordPress Plugin</h2>
-              <p>Enroll Now</p>
+              <img src="images/css.jpg" alt="Image placeholder">
+              <h2>CSS 3</h2>
+              <p>Vinicius Rocha</p>
+            </a>
+          </div>
+          <div class="col-lg-2 col-md-4 col-sm-6 col-12">
+            <a href="#" class="course">
+              <img src="images/bootstrap.jpg" alt="Image placeholder">
+              <h2>Bootstrap 4</h2>
+              <p>Raniery Peixoto</p>
             </a>
           </div>
           <div class="col-lg-2 col-md-4 col-sm-6 col-12">
             <a href="#" class="course">
               <img src="images/javascript.jpg" alt="Image placeholder">
-              <h2>JavaScript 101</h2>
-              <p>Enroll Now</p>
+              <h2>JavaScript</h2>
+              <p>Welington Andrade</p>
             </a>
           </div>
           <div class="col-lg-2 col-md-4 col-sm-6 col-12">
             <a href="#" class="course">
-              <img src="images/photoshop.jpg" alt="Image placeholder">
-              <h2>Photoshop Design 101</h2>
-              <p>Enroll Now</p>
+              <img src="images/git.jpg" alt="Image placeholder">
+              <h2>Git</h2>
+              <p>Welington Andrade</p>
             </a>
           </div>
           <div class="col-lg-2 col-md-4 col-sm-6 col-12">
             <a href="#" class="course">
-              <img src="images/reactjs.jpg" alt="Image placeholder">
-              <h2>Learn Native ReactJS</h2>
-              <p>Enroll Now</p>
-            </a>
-          </div>
-          <div class="col-lg-2 col-md-4 col-sm-6 col-12">
-            <a href="#" class="course">
-              <img src="images/angularjs.jpg" alt="Image placeholder">
-              <h2>Learn AngularJS 2</h2>
-              <p>Enroll Now</p>
+              <img src="images/laravel.jpg" alt="Image placeholder">
+              <h2>Laravel</h2>
+              <p>Nathália Ferreira</p>
             </a>
           </div>
         </div>
@@ -158,183 +158,78 @@
     </section>
     <!-- END section -->
 
-   
+      <section class="site-section">
+          <div class="container">
+              <div class="row justify-content-center mb-5">
+                  <div class="col-md-7 text-center">
+                      <h2>Conheça seus professores</h2>
+                      <p class="lead"></p>
+                  </div>
+              </div>
+              <section class="school-features text-dark d-flex">
 
-    <section class="site-section">
-      <div class="container">
-        <div class="row justify-content-center mb-5">
-          <div class="col-md-7 text-center">
-            <h2>Meet Your Instructors</h2>
-            <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum magnam illum maiores adipisci pariatur, eveniet.</p>
+                  <div class="inner">
+                      <div class="media d-block feature text-center">
+                          <img src="images/person_1.jpg" alt="Image placeholder" class="mb-3">
+                          <div class="media-body">
+                              <h3 class="mt-0">Nathália Ferreira</h3>
+                              <p class="instructor-meta">Laravel</p>
+                              <p>Nathália ira instruir você a como criar um banco de dados utilizando o Laravel e Laravel com ACL.</p>
+                          </div>
+                      </div>
+
+                      <div class="media d-block feature text-center">
+                          <img src="images/person_2.jpg" alt="Image placeholder" class="mb-3">
+                          <div class="media-body">
+                              <h3 class="mt-0">Raniery Peixoto</h3>
+                              <p class="instructor-meta">Bootstrap 4</p>
+                              <p>Raniery ira instruir você a como criar um site responsivo, disponivel para todas as plataformas.</p>
+                          </div>
+                      </div>
+
+                      <div class="media d-block feature text-center">
+                          <img src="images/person_3.jpg" alt="Image placeholder" class="mb-3">
+                          <div class="media-body">
+                              <h3 class="mt-0">Vinicius Rocha</h3>
+                              <p class="instructor-meta">HTML 5 / CSS 3</p>
+                              <p>Vinicius ira instruir você a como criar um site do 0 utilizando comandos do HTML e usando os estilos do CSS.</p>
+                          </div>
+                      </div>
+
+
+                      <div class="media d-block feature text-center">
+                          <img src="images/person_4.jpg" alt="Image placeholder" class="mb-3">
+                          <div class="media-body">
+                              <h3 class="mt-0">Welington Andrade</h3>
+                              <p class="instructor-meta">Git / JavaScript</p>
+                              <p>Welington ira instruir você a como utilizar o controle de versão do Git e também a usar scripts do JavaScript.</p>
+                          </div>
+                      </div>
+                  </div>
+              </section>
           </div>
-        </div>
-        <section class="school-features text-dark d-flex">
+      </section>
 
-          <div class="inner">
-            <div class="media d-block feature text-center">
-              <img src="images/person_1.jpg" alt="Image placeholder" class="mb-3">
-              <div class="media-body">
-                <h3 class="mt-0">Rhea Smith</h3>
-                <p class="instructor-meta">WordPress Expert</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora fuga suscipit numquam esse saepe quam, eveniet iure assumenda dignissimos aliquam!</p>
-              </div>
-            </div>
-
-            <div class="media d-block feature text-center">
-              <img src="images/person_2.jpg" alt="Image placeholder" class="mb-3">
-              <div class="media-body">
-                <h3 class="mt-0">Gregg White</h3>
-                <p class="instructor-meta">HTML4, CSS3 Expert</p>
-                <p>Delectus fuga voluptatum minus amet, mollitia distinctio assumenda voluptate quas repellat eius quisquam odio. Aliquam, laudantium, optio? Error velit, alias.</p>
-              </div>
-            </div>
-
-            <div class="media d-block feature text-center">
-              <img src="images/person_3.jpg" alt="Image placeholder" class="mb-3">
-              <div class="media-body">
-                <h3 class="mt-0">Rob Gold</h3>
-                <p class="instructor-meta">JS Expert</p>
-                <p>Delectus fuga voluptatum minus amet, mollitia distinctio assumenda voluptate quas repellat eius quisquam odio. Aliquam, laudantium, optio? Error velit, alias.</p>
-              </div>
-            </div>
-
-
-            <div class="media d-block feature text-center">
-              <img src="images/person_4.jpg" alt="Image placeholder" class="mb-3">
-              <div class="media-body">
-                <h3 class="mt-0">Wennie Poe</h3>
-                <p class="instructor-meta">Angular JS Expert</p>
-                <p>Harum, adipisci, aspernatur. Vero repudiandae quos ab debitis, fugiat culpa obcaecati, voluptatibus ad distinctio cum soluta fugit sed animi eaque?</p>
-              </div>
-            </div>
-          </div>
-        </section>
-
-        <section class="school-features text-dark last d-flex">
-
-          <div class="inner">
-            <div class="media d-block feature text-center">
-              <img src="images/person_1.jpg" alt="Image placeholder" class="mb-3">
-              <div class="media-body">
-                <h3 class="mt-0">Rhea Smith</h3>
-                <p class="instructor-meta">WordPress Expert</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora fuga suscipit numquam esse saepe quam, eveniet iure assumenda dignissimos aliquam!</p>
-              </div>
-            </div>
-
-            <div class="media d-block feature text-center">
-              <img src="images/person_2.jpg" alt="Image placeholder" class="mb-3">
-              <div class="media-body">
-                <h3 class="mt-0">Gregg White</h3>
-                <p class="instructor-meta">Photoshop Expert</p>
-                <p>Delectus fuga voluptatum minus amet, mollitia distinctio assumenda voluptate quas repellat eius quisquam odio. Aliquam, laudantium, optio? Error velit, alias.</p>
-              </div>
-            </div>
-
-            <div class="media d-block feature text-center">
-              <img src="images/person_3.jpg" alt="Image placeholder" class="mb-3">
-              <div class="media-body">
-                <h3 class="mt-0">Rob Gold</h3>
-                <p class="instructor-meta">Web Design Expert</p>
-                <p>Delectus fuga voluptatum minus amet, mollitia distinctio assumenda voluptate quas repellat eius quisquam odio. Aliquam, laudantium, optio? Error velit, alias.</p>
-              </div>
-            </div>
-
-
-            <div class="media d-block feature text-center">
-              <img src="images/person_4.jpg" alt="Image placeholder" class="mb-3">
-              <div class="media-body">
-                <h3 class="mt-0">Wennie Poe</h3>
-                <p class="instructor-meta">React JS Expert</p>
-                <p>Harum, adipisci, aspernatur. Vero repudiandae quos ab debitis, fugiat culpa obcaecati, voluptatibus ad distinctio cum soluta fugit sed animi eaque?</p>
-              </div>
-            </div>
-          </div>
-        </section>
-
-
-      </div>
-    </section>
     <!-- END section -->
 
     <section class="section-cover bg-dark">
       <div class="container">
         <div class="row justify-content-center align-items-center intro">
           <div class="col-md-7 text-center element-animate">
-            <h2>Sign Up And Get a 7-day Free Trial</h2>
-            <p class="lead mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto quidem tempore expedita facere facilis, dolores!</p>
-            <p><a href="#" class="btn btn-primary">Sign up and get a 7-day free trial</a></p>
+            <h2>Cadastre-se e ganhe 7 dias grátis</h2>
+            <p class="lead mb-5"> Inscreva-se para receber os cursos disponiveis</p>
+            <p><a href="#" class="btn btn-primary">Cadastre-se</a></p>
           </div>
         </div>
       </div>
     </section>
     <!-- END section -->
 
-
-    
-  
-    <footer class="site-footer" style="background-image: url(images/big_image_3.jpg);">
-      <div class="container">
-        <div class="row mb-5">
-          <div class="col-md-4">
-            <h3>About</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi, accusantium optio unde perferendis eum illum voluptatibus dolore tempora, consequatur minus asperiores temporibus reprehenderit.</p>
-          </div>
-          <div class="col-md-6 ml-auto">
-            <div class="row">
-              <div class="col-md-4">
-                <ul class="list-unstyled">
-                  <li><a href="#">About Us</a></li>
-                  <li><a href="#">Company</a></li>
-                  <li><a href="#">Teachers</a></li>
-                  <li><a href="#">Courses</a></li>
-                  <li><a href="#">Categories</a></li>
-                </ul>
-              </div>
-              <div class="col-md-4">
-                <ul class="list-unstyled">
-                  <li><a href="#">About Us</a></li>
-                  <li><a href="#">Company</a></li>
-                  <li><a href="#">Teachers</a></li>
-                  <li><a href="#">Courses</a></li>
-                  <li><a href="#">Categories</a></li>
-                </ul>
-              </div>
-              <div class="col-md-4">
-                <ul class="list-unstyled">
-                  <li><a href="#">About Us</a></li>
-                  <li><a href="#">Company</a></li>
-                  <li><a href="#">Teachers</a></li>
-                  <li><a href="#">Courses</a></li>
-                  <li><a href="#">Categories</a></li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md-12">
-            <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
-          </div>
-        </div>
-      </div>
-    </footer>
-    <!-- END footer -->
-    
-    <!-- loader -->
-    <div id="loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#f4b214"/></svg></div>
-
-    <script src="js/jquery-3.2.1.min.js"></script>
-    <script src="js/jquery-migrate-3.0.0.js"></script>
-    <script src="js/popper.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/owl.carousel.min.js"></script>
-    <script src="js/jquery.waypoints.min.js"></script>
-    <script src="js/jquery.stellar.min.js"></script>
-
-    
-    <script src="js/main.js"></script>
+      <?php
+      include_once "layout/footer.php";
+      ?>
+      <?php
+      include_once "layout/script.php";
+      ?>
   </body>
 </html>
